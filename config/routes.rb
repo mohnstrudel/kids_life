827c510792d4c82608de84a55f53a23121f7b2b6
Anton Kostin <anton.kostin@mhm-hr.com>
2019-09-26 15:06:37 +0200

    Initial Kids Life commit.

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :front do
+    root 'static_pages#home'
+  end
+end
