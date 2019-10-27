82b53f5fb567ce60b6a0e88e7a5c5716e5904e24
Anton Kostin <anton.kostin@mhm-hr.com>
2019-10-27 23:59:56 +0100

    KLF-007: First styling changes (fonts, events, contact data).

--- app/javascript/packs/styles.js
+++ app/javascript/packs/styles.js
@@ -1,3 +1,5 @@
 // app/javascripts/packs/styles.js
 // import 'font-awesome/scss/font-awesome.scss';
 // import 'multiselect/css/multi-select.css';
+
+// import url('https://fonts.googleapis.com/css?family=Amatic+SC&display=swap');
