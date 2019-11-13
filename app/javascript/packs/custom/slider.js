13732112d800bfa4265ad0ac62f436ed7412d750
Anton Kostin <anton.kostin@mhm-hr.com>
2019-11-13 22:43:30 +0100

    KLF-012: Слайдер, лого, навигация, метатэги.

--- app/javascript/packs/custom/slider.js
+++ app/javascript/packs/custom/slider.js
@@ -1,36 +1,12 @@
 $(document).ready(function(){
-  // console.log('slider is loaded');
-  var slideIndex = 1;
-  showSlides(slideIndex);
-
-  // Next/previous controls
-  $('.slideshow-container a').on('click', function(e){
-    var next_move = $(this).data("nextmove");
-    showSlides(slideIndex += next_move);
-  });
-  // function plusSlides(n) {
-  //   showSlides(slideIndex += n);
-  // }
-
-  // Thumbnail image controls
-
-  $('.dot').on('click', function(e){
-    var slide_count = $(this).data("currentslide");
-    showSlides(slideIndex = slide_count);
-  });
-
-  // function currentSlide(n) {
-  //   console.log("current Slide function clicked");
-  //   showSlides(slideIndex = n);
-  // }
-
-  function showSlides(n) {
-    // console.log("show Slides is executed");
+  var showSlidesFunc = function showSlides(n, increment) {
+    increment_slide_index();
     var i;
     var slides = $(".mySlides");
     var dots = $(".dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -39,5 +15,29 @@ $(document).ready(function(){
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " slider_active";
+    setTimeout(showSlidesFunc, 4000);
+  };
+
+  function increment_slide_index(){
+    if((slideIndex += 1) > 4) {
+      slideIndex = 1;
+    }
   }
+
+
+  var slideIndex = 0;
+  // showSlides(slideIndex);
+  showSlidesFunc(1);
+
+  // Next/previous controls
+  // $('.slideshow-container a').on('click', function(e){
+  //   var next_move = $(this).data("nextmove");
+  //   showSlidesFunc(slideIndex += next_move, false);
+  // });
+
+  // Thumbnail image controls
+  // $('.dot').on('click', function(e){
+  //   var slide_count = $(this).data("currentslide");
+  //   showSlidesFunc(slideIndex = slide_count, false);
+  // });
 });
